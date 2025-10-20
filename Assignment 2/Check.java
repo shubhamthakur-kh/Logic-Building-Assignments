08ce04e9fa6350559fd6b6871d39ed1c4a9c29ed
Shubham Thakur <shubham.thakur.cmaug25@gmail.com>
2025-10-20 12:58:10 +0530

    Add files via upload

---
+++ Assignment 2/Check.java
@@ -0,0 +1,22 @@
+public class Check {
+    public static void main(String[] args) {
+        
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 9L;
+        float e = 8.0f;
+        double f = 2.0;
+        char g = 'S';
+        boolean h = true;
+		
+        System.out.println("byte a = " + a);
+        System.out.println("short b = " + b);
+        System.out.println("int c = " + c);
+        System.out.println("long d = " + d);
+        System.out.println("float e = " + e);
+        System.out.println("double f = " + f);
+        System.out.println("char g = " + g);
+        System.out.println("boolean h = " + h);
+    }
+}
---
+++ Assignment 2/SwitchCase.java
@@ -0,0 +1,31 @@
+class SwitchCase{
+	public static void main (String args[]){
+		int day = 3;
+		switch(day){
+			case 1 :
+			System.out.println("The Day is Monday");
+			break;
+			case 2 :
+			System.out.println("The Day is Tuesday");
+			break;
+			case 3 : 
+			System.out.println("The Day is wednesday");
+			break;
+			case 4 :
+			System.out.println("The Day is Thrusday");
+			break;
+			case 5 :
+			System.out.println("The Day is Friday");
+			break;
+			case 6 : 
+			System.out.println("The Day is Saturday");
+			break;
+			case 7 :
+			System.out.println("The Day is Sunday");
+			break;
+			default : 
+			System.out.println("Invalid Day Number");
+		}
+		
+	}
+}
